09822a4057523014769c1b57f4e16a7b0194b7d0
Severyn Kozak <severyn.kozak@gmail.com>
2014-12-22 10:39:10 -0500

    Integrate Pelias elasticsearch pipeline.
    import.js
    	-Integrate the Pelias elasticsearch pipeline, abstracting it
    	away to a `createPeliasElasticsearchPipeline()` function.
    
    package.json
    	-Add a `package.json` for this package.
    	-Add Pelias elasticsearch dependencies.

--- import.js
+++ import.js
@@ -2,22 +2,50 @@
  * @file Entry-point script for the TIGER import pipeline.
  */
 
+var through = require( 'through2' );
+var peliasDbclient = require( 'pelias-dbclient' );
+var peliasSuggesterPipeline = require( 'pelias-suggester-pipeline' );
+
 function importTigerDir( path ){
-	console.log( 'Importing: ', path );
+  console.log( 'Importing: ', path );
+}
+
+/**
+ * Create the Pelias elasticsearch import pipeline.
+ *
+ * @return {Writable stream} The pipeline entrypoint; Document records should
+ *    be written to it.
+ */
+function createPeliasElasticsearchPipeline(){
+  var dbclientMapper = through.obj( function( model, enc, next ){
+    this.push({
+      _index: 'pelias',
+      _type: model.getType(),
+      _id: model.getId(),
+      data: model
+    });
+    next();
+  });
+
+  var entryPoint = peliasSuggesterPipeline.pipeline;
+  entryPoint
+    .pipe( dbclientMapper )
+    .pipe( peliasDbclient() );
+  return entryPoint;
 }
 
 /**
  * Handle the command-line arguments passed to the script.
  */
 function handleUserArgs( argv ){
-	var usageMessage = 'TODO: usage message.';
-	if( argv.length !== 1 ){
-		console.error( usageMessage );
-		process.exit( 1 );
-	}
-	else {
-		importTigerDir( argv[ 0 ] );
-	}
+  var usageMessage = 'TODO: usage message.';
+  if( argv.length !== 1 ){
+    console.error( usageMessage );
+    process.exit( 1 );
+  }
+  else {
+    importTigerDir( argv[ 0 ] );
+  }
 }
 
 handleUserArgs( process.argv.slice( 2 ) );
