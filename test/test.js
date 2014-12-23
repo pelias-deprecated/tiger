2a7a58471df4535ce23a4f3987b0a699be9b99c8
Severyn Kozak <severyn.kozak@gmail.com>
2014-12-23 11:54:43 -0500

    Add unit-testing infrastructure.
    package.json
    	-Add unit-testing dependencies; bump package version.
    
    test/test.js
    	-Add the core unit-testing module.
    	-Contains empty test functions, to be defined in a future
    	commit.

---
+++ test/test.js
@@ -0,0 +1,16 @@
+/**
+ * @file The main entry point for the TIGER importer's unit-tests.
+ */
+
+'use strict';
+
+var tape = require( 'tape' );
+var createAdminValues = require( '../lib/create_record_stream' );
+
+tape( 'createRecordStream.create(): ', function ( test ){
+  test.end(  );
+});
+
+tape( 'createAdminValues.createAdminValues(): ', function ( test ){
+  test.end(  );
+});
