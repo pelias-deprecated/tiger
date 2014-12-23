e3a3e40533d55cd5c186ecfdd83fab32a12268b7
Severyn Kozak <severyn.kozak@gmail.com>
2014-12-23 12:03:09 -0500

    Add unit test for createAdminValues().
    lib/create_record_stream.js, test/test.js
    	-Expose `createAdminValues()` in the `create_record_stream`
    	module, and define a unit-test for it in `test`.

--- test/test.js
+++ test/test.js
@@ -5,12 +5,34 @@
 'use strict';
 
 var tape = require( 'tape' );
-var createAdminValues = require( '../lib/create_record_stream' );
+var createRecordStream = require( '../lib/create_record_stream' );
 
 tape( 'createRecordStream.create(): ', function ( test ){
   test.end(  );
 });
 
-tape( 'createAdminValues.createAdminValues(): ', function ( test ){
-  test.end(  );
+tape( 'createRecordStream.createAdminValues(): ', function ( test ){
+  var testCases = {
+    '01001': {
+      country: 'United States',
+      state: 'Alabama',
+      county: 'Autauga'
+    },
+    '36061': {
+      country: 'United States',
+      state: 'New York',
+      county: 'New York'
+    },
+    '23001': {
+      country: 'United States',
+      state: 'Maine',
+      county: 'Androscoggin'
+    }
+  };
+
+  test.plan( Object.keys( testCases ).length );
+  for( var arg in testCases ){
+    var actual = createRecordStream.createAdminValues( arg );
+    test.deepEqual( actual, testCases[ arg ] );
+  }
 });
